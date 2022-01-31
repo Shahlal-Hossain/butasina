21f64bd3c1441c0c2069e959bcd593a098a6c608
Shahlal-hossain <shahlalhossain154@gmail.com>
2022-01-31 22:50:52 +0300

    secoun commit

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function hendelTicket(ticketClass,isIncrease){
-    let ticket = document.getElementById(ticketClass).innerText;
-    let ticketInNumber = parseInt(ticket);
+    const ticket = document.getElementById(ticketClass);
+    const ticketInNumber = parseInt(ticket.innerText);
     let totalTicket = ticketInNumber;
     if(isIncrease == true){
-        totalTicket = ticketInNumber++;
+        totalTicket = ticketInNumber+ 1;
     }
     if(isIncrease == false && totalTicket > 0){
-        totalTicket = ticketInNumber -1;
+        totalTicket = ticketInNumber - 1;
     }
     ticket.innerText = totalTicket;
+    calculator()
+}
+function calculator(){
+    const first = parseInt(document.getElementById('first').innerText)
+    const business = parseInt(document.getElementById('business').innerText)
+    const economy = parseInt(document.getElementById('economy').innerText)
+    const preminmEconomy = parseInt(document.getElementById('preminm-economy').innerText)
+    const totalAmount = first * 60500 + business * 50500 + economy * 40500 + preminmEconomy * 30500;
+    document.getElementById('sub-total').innerText = totalAmount;
+    const vat = totalAmount * 0.05;
+    document.getElementById('vat').innerText = vat;
+    const total = totalAmount + vat ;
+    document.getElementById('total').innerText = total;
+}
+function bookTicket(){
+    document.getElementById('main').style.display = "none";
+    document.getElementById('main-input').style.display = "block";
 }
